522b49d559fa24b05c8a2cb129fd128515acd1d0
Karthik Chikmagalur <karthikchikmagalur@gmail.com>
2019-09-01 10:48:51 -0700

    Added 9th i3 workspace. External editor editing in Qutebrowser.

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -821,6 +821,7 @@ c.content.autoplay = False
 ## `{line0}`: Same as `{line}`, but starting from index 0. * `{column0}`:
 ## Same as `{column}`, but starting from index 0.
 ## Type: ShellCommand
+c.editor.command = ['emacsclient', '-c', '{file}']
 # c.editor.command = ['gvim', '-f', '{file}', '-c', 'normal {line}G{column0}l']
 
 ## Encoding to use for the editor.
